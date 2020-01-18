fac3ec6b3d9a7be6cd699e1a200a0f682aae7b60
Martin Haye <martin.haye@ucop.edu>
2020-01-17 16:34:58 -0800

    Began fleshing out the db schema for flexible metadata.

---
+++ migrations/0035_metaflex.rb
@@ -0,0 +1,63 @@
+require 'json'
+
+Sequel.migration do
+  up do
+    create_table(:formats) do
+      String :id, primary_key: true, null: false
+      String :descrip, null: false
+    end
+
+    from(:formats).insert(id: 'text',         descrip: "Plain string or number, no HTML")
+    from(:formats).insert(id: 'choice',       descrip: "Constrained string (or number) from a list of choices")
+    from(:formats).insert(id: 'html',         descrip: "HTML-formatted text")
+    from(:formats).insert(id: 'date',         descrip: "A date (YYYY-MM-DD)")
+    from(:formats).insert(id: 'boolean',      descrip: "True/Yes or False/No")
+    from(:formats).insert(id: 'person_list',  descrip: "One or more people (or organizations)")
+    from(:formats).insert(id: 'text_list',    descrip: "One or more plain text items, e.g. keywords")
+    from(:formats).insert(id: 'choice_list',  descrip: "One or more strings from a list of choices")
+    from(:formats).insert(id: 'isbn',         descrip: "International Standard Book Number (ISBN-10 or ISBN-13)")
+    from(:formats).insert(id: 'issn',         descrip: "International Standard Serial Number")
+    from(:formats).insert(id: 'url',          descrip: "Uniform Resource Locator (on the web)")
+    from(:formats).insert(id: 'doi',          descrip: "Digital Object Identifier")
+    from(:formats).insert(id: 'pagination',   descrip: "First and last page")
+    from(:formats).insert(id: 'data_avail',   descrip: "Data availability statement")
+    from(:formats).insert(id: 'embargo_date', descrip: "Date to release embargo, in the near future")
+    from(:formats).insert(id: 'rights',       descrip: "Either 'public' or a Creative Commons license")
+    from(:formats).insert(id: 'image',        descrip: "An image (e.g. cover image)")
+    from(:formats).insert(id: 'id_list',      descrip: "List of typed identifiers (e.g. local_ids)")
+
+    create_table(:fields) do
+      String :id, primary_key: true, null: false
+      foreign_key :format_id, :formats, type: String, null: false
+      String :attrs, :type=>'JSON'
+    end
+
+    from(:fields).insert(id: 'title', format_id: 'html',
+      attrs: { name: "Title", descrip: "Official title of the publication", is_always_required: true }.to_json )
+    from(:fields).insert(id: 'published', format_id: 'date',
+      attrs: { name: "Publication Date", descrip: "Date this was published", is_always_required: true }.to_json )
+
+    create_table(:pubtypes) do
+      String :id, primary_key: true, null: false
+      foreign_key :cloned_from, :pubtypes, type: String
+      String :attrs, :type=>'JSON'
+    end
+
+    create_table(:pubtype_fields) do
+      primary_key :id
+      foreign_key :pubtype_id, :pubtypes, type: String, null: false
+      foreign_key :field_id, :fields, type: String, null: false
+      Integer :ordering, :null=>false
+      String :attrs, :type=>'JSON'
+      index [:pubtype_id, :ordering], unique: true
+      index [:pubtype_id, :field_id], unique: true
+    end
+  end
+
+  down do
+    drop_table(:pubtype_fields)
+    drop_table(:pubtypes)
+    drop_table(:fields)
+    drop_table(:formats)
+  end
+end
